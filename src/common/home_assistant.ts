9a64f0d2f1b0f8640e5c4224547c265e238564c6
Matchlighter <ml@matchlighter.net>
2023-02-17 17:06:54 -0700

    checkin

--- src/common/home_assistant.ts
+++ src/common/home_assistant.ts
@@ -1,4 +1,16 @@
+import { ResumablePromise, SerializedResumable } from "src/runtime/resumable_promise";
 
 export class HomeAssistantIntegration {
-    
+    for_event(event: string) {
+        
+    }
+}
+
+class EventAwaiter extends ResumablePromise<any>{
+    serialize(): SerializedResumable {
+        return {
+            type: 'ha_event_waiter',
+            sideeffect_free: true,
+        }
+    }
 }
