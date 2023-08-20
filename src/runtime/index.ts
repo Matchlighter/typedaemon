ca2c79f5f7d9bc94ab92f0d94105f34e2b12c31a
Ethan Knapp <ethan@matchlighter.net>
2023-08-19 19:50:28 -0600

    add persistent storage

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -4,3 +4,5 @@ export { resumable } from './resumable'
 export * as schedule from './hooks/schedule'
 
 export { Application } from './application'
+
+export { persistent } from './hooks/persistence'
